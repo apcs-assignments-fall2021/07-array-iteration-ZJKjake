b4de27920459689227a58398c153f1b1825923cd
jakezhang <24jzhang@athenian.org>
2021-09-22 17:15:45 -0700

    first try...

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -1,15 +1,37 @@
+import java.util.Arrays;
+
 public class MyMain {
 
     // Reverses an array
     public static int[] reverse(int[] arr) {
-        // REPLACE WITH YOUR CODE HERE
-        return null;
+        int length=arr.length;
+
+        int[] arr2=new int[length];
+        int x=0;
+        for (int i=arr.length-1;i>=0;i--){
+            arr2[x]=arr[i];
+            if (x+1>arr.length){
+                break;
+            }
+            x+=1;
+        }
+        return arr2;
     }
 
     // Finds the second largest number in an array
     public static int secondLargest(int[] arr) {
-        // REPLACE WITH YOUR CODE HERE
-        return -1;
+
+        for (int i=0;i<arr.length;i++){
+            for (int j=i+1;j<arr.length;j++){
+                if (arr[i]>arr[j]) {
+                    int n = arr[i];
+                    arr[i] = arr[j];
+                    arr[j]=n;
+                }
+            }
+
+        }
+        return arr[arr.length-2];
     }
 
     // This method is given a word, and it checks to see
@@ -18,16 +40,24 @@ public class MyMain {
     // The method returns true is the word is spelled correctly
     // and false otherwise
     public static boolean spellCheck(String word) {
-        // Don't edit this array
-        // This is our dictionary of valid words
-        String[] dictionary = {"the","of","and","a","to","in","is","you","that","it","he","was","for","on","are","as","with","his","they","I","at","be","this","have","from","or","one","had","by","word","but","not","what","all","were","we","when","your","can","said","there","use","an","each","which","she","do","how","their","if","will","up","other","about","out","many","then","them","these","so","some","her","would","make","like","him","into","time","has","look","two","more","write","go","see","number","no","way","could","people","my","than","first","water","been","call","who","oil","its","now","find","long","down","day","did","get","come","made","may","cat","dog","cats","dogs"};
 
+        String[] dictionary = {"the", "of", "and", "a", "to", "in", "is", "you", "that", "it", "he", "was", "for", "on", "are", "as", "with", "his", "they", "I", "at", "be", "this", "have", "from", "or", "one", "had", "by", "word", "but", "not", "what", "all", "were", "we", "when", "your", "can", "said", "there", "use", "an", "each", "which", "she", "do", "how", "their", "if", "will", "up", "other", "about", "out", "many", "then", "them", "these", "so", "some", "her", "would", "make", "like", "him", "into", "time", "has", "look", "two", "more", "write", "go", "see", "number", "no", "way", "could", "people", "my", "than", "first", "water", "been", "call", "who", "oil", "its", "now", "find", "long", "down", "day", "did", "get", "come", "made", "may", "cat", "dog", "cats", "dogs"};
+        for (String x : dictionary) {
+            if (x == word) {
+                return true;
+            }
+        }
         // REPLACE WITH YOUR CODE HERE
         return false;
     }
 
 
+
     public static void main(String[] args) {
-        // YOUR CODE HERE
+        System.out.println(Arrays.toString(MyMain.reverse(new int[] {1,3,4,2,5})));
+        System.out.println(MyMain.secondLargest(new int[] {122,123,124,125,126,1}));
+        System.out.println(MyMain.spellCheck("Jake"));
+        System.out.println(MyMain.spellCheck("I'm hungry"));
+        System.out.println(MyMain.spellCheck("dogs"));
     }
 }
